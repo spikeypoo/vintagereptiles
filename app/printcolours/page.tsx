4e165ab4de747f090d1998d8bd796515eecd0058
spikeypoo <>
2025-10-07 20:50:25 -0400

    add colour

--- app/printcolours/page.tsx
+++ app/printcolours/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   useEffect(() => {
     const imageList = [];
-    for (let i = 1; i <= 35; i++) {
+    for (let i = 1; i <= 36; i++) {
       imageList.push(`/images/3dprintcolours/${i}.jpg`);
     }
     setImages(imageList);
